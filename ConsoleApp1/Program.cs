92d4b5e8abf0c4651945bb9ea98475c3fa67a213
svsjo <79276639+svsjo@users.noreply.github.com>
2021-09-07 11:32:16 +0200

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
